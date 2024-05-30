d36e40697867504450dd4e85909cec40a151477f
Zbuddy13 <jzshaw13@gmail.com>
2024-05-30 17:50:03 -0400

    Created script for a runner in a container

---
+++ prep.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+FILENAME=actions-runner
+PREP="prep.sh"
+
+cd /data
+
+# Install Python
+if ! python3; then
+    echo "INFO: Installing Python"
+    apt update
+    apt install python3
+fi
+
+if ! python3; then
+    echo "ERROR: Python cannot be installed"
+fi
+
+# Install docker
+if ! docker --version; then
+    echo "INFO: Installing Docker"
+    # Add Docker's official GPG key:
+    apt-get update
+    apt-get install ca-certificates curl
+    install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    # Add the repository to Apt sources:
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	
+    if ! docker --version; then
+        echo "ERROR: Docker install failed"
+        exit 1
+fi
+
+# Attempt to run a container if not in a container
+if [ ! -f /.dockerenv  ]; then
+	if ! docker run --rm hello-world; then
+		echo "ERROR: Could not get docker to run the hello world container"
+		exit 2
+	fi
+fi
+
+echo "INFO: Successfully verified docker installation!"
+
+# Create a folder
+if [ ! -e "$FILENAME" ]; then
+    # Make directory
+    mkdir actions-runner && cd actions-runner
+    # Download the latest runner package
+    curl -o actions-runner-linux-x64-2.316.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.316.1/actions-runner-linux-x64-2.316.1.tar.gz
+    # Optional: Validate the hash
+    echo "392b9d7b6d5b4d4f3814dbf874641b894f0c72447cdf05ce93158832d2d49b6b  actions-runner-osx-x64-2.316.1.tar.gz" | shasum -a 256 -c
+    # Extract the installer
+    tar xzf ./actions-runner-osx-x64-2.316.1.tar.gz
+    # Config runner
+    ./config.sh --url $URL --token $RUNNER_TOKEN
+fi
+
+if [ ! -e "$PREP" ]; then
+    echo "ERROR Runner install failed"
+    exit 3
+fi
+
+echo "INFO: Runner Successfuly Installed!"
+
+chmod +x run.sh
+source run.sh
